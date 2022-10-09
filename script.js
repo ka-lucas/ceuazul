2947137fef96ba27ca7df68dbc633fab2c363b41
Lais Leão <114625780+laisleao@users.noreply.github.com>
2022-10-09 14:25:34 -0300

    alteração

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+//código carousel 
+let count = 1;
+document.getElementById("radio1").checked = true;
+setInterval(function(){
+    nextImage();
+ }, 5000)
+function nextImage{
+  count++;
+ if(count>4){
+    count = 1;
+}
+  document.getElementById("radio" + count).checked = true;
+}
+//fim código carousel 
